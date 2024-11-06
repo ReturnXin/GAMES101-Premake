4ab8555f44e9a3a8e84d3895ab94d90ab084b83e
ReturnXin <1392008423@qq.com>
2024-11-06 11:09:02 +0800

    feat: 实现多线程

--- Frame/Source/Assignment/Assignment7/Renderer.cpp
+++ Frame/Source/Assignment/Assignment7/Renderer.cpp
@@ -3,6 +3,8 @@
 //
 
 #include <fstream>
+#include <thread>
+#include <mutex>
 #include "Scene.hpp"
 #include "Renderer.hpp"
 
@@ -22,26 +24,46 @@ void Renderer::Render(const Scene& scene)
     float scale = tan(deg2rad(scene.fov * 0.5));
     float imageAspectRatio = scene.width / (float)scene.height;
     Vector3f eye_pos(278, 273, -800);
-    int m = 0;
 
     // change the spp value to change sample ammount
     int spp = 8;
+	int thread_num = 8;
+    int thread_height = scene.height / thread_num;
+    std::vector<std::thread> threads(thread_num);
     std::cout << "SPP: " << spp << "\n";
-    for (uint32_t j = 0; j < scene.height; ++j) {
-        for (uint32_t i = 0; i < scene.width; ++i) {
-            // generate primary ray direction
-            float x = (2 * (i + 0.5) / (float)scene.width - 1) *
-                      imageAspectRatio * scale;
-            float y = (1 - 2 * (j + 0.5) / (float)scene.height) * scale;
 
-            Vector3f dir = normalize(Vector3f(-x, y, 1));
-            for (int k = 0; k < spp; k++){
-                framebuffer[m] += scene.castRay(Ray(eye_pos, dir), 0) / spp;  
-            }
-            m++;
-        }
-        UpdateProgress(j / (float)scene.height);
+    std::mutex mtx;
+    float process = 0;
+    float Reciprocal_Scene_height = 1.f / (float)scene.height;
+    auto castRay = [&](int thread_index)
+        {
+            int height = thread_height * (thread_index + 1);
+            for (uint32_t j = height - thread_height; j < height; ++j) {
+                for (uint32_t i = 0; i < scene.width; ++i) {
+                    // generate primary ray direction
+                    float x = (2 * (i + 0.5) / (float)scene.width - 1) *
+                        imageAspectRatio * scale;
+                    float y = (1 - 2 * (j + 0.5) / (float)scene.height) * scale;
+
+                    Vector3f dir = normalize(Vector3f(-x, y, 1));
+                    for (int k = 0; k < spp; k++) {
+                        framebuffer[j * scene.width + i] += scene.castRay(Ray(eye_pos, dir), 0) / spp;
+                    }
+                }
+                mtx.lock();
+                process = process + Reciprocal_Scene_height;
+                UpdateProgress(j / (float)scene.height);
+                mtx.unlock();
+			}
+        };
+
+    for (int k = 0; k < thread_num; k++) {
+        threads[k] = std::thread(castRay, k);
     }
+    for (int k = 0; k < thread_num; k++) {
+        threads[k].join();
+    }
+
     UpdateProgress(1.f);
 
     // save framebuffer to file
